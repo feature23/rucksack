8610997c8fa544fe3b43d6b739e9d56a8b2b56f9
James Blair <jblair@feature23.com>
2024-08-09 16:05:21 -0600

    Improve formatting of output and add simple progress bar. #4

---
+++ Rucksack.ConsoleExample/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Rucksack;
+using Rucksack.LoadStrategies;
+
+int completedTasks = 0;
+
+await LoadTestRunner.Run(() =>
+{
+    var index = Interlocked.Increment(ref completedTasks);
+
+    return index switch
+    {
+        _ when index % 3 == 0 => Task.FromException(new ArgumentOutOfRangeException("arg", "Bad argument!")),
+        _ when index % 5 == 0 => Task.FromException(new InvalidOperationException("Invalid operation!")),
+        _ => Task.Delay(TimeSpan.FromSeconds(1.1))
+    };
+}, new LoadTestOptions
+{
+    LoadStrategy = new SequentialLoadStrategy(TimeSpan.FromSeconds(1))
+    {
+        new OneShotLoadStrategy(10),
+        new OneShotLoadStrategy(20),
+        new OneShotLoadStrategy(15),
+        new OneShotLoadStrategy(25),
+        new OneShotLoadStrategy(30)
+    }
+});
--- Rucksack/LoadTestRunner.cs
+++ Rucksack/LoadTestRunner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.Extensions.Logging;
+using Spectre.Console;
 
 namespace Rucksack;
 
@@ -14,54 +15,66 @@ public static class LoadTestRunner
 
     public static async Task Run(Func<Task> action, LoadTestOptions options)
     {
-        var loggerFactory = options.LoggerFactory ?? LoggerFactory.Create(builder =>
-        {
-            builder.AddConsole().SetMinimumLevel(LogLevel.Information);
-        });
+        var logger = options.LoggerFactory?.CreateLogger(nameof(LoadTestRunner));
 
-        var logger = loggerFactory.CreateLogger(nameof(LoadTestRunner));
-
-        logger.LogInformation("Rucksack is running...");
+        logger?.LogInformation("Rucksack is running...");
+        AnsiConsole.Write(new FigletText("Rucksack")
+            .Color(Color.Green));
+        AnsiConsole.WriteLine("Starting load test...");
 
         LoadStrategyResult? result = null;
         List<Task<LoadTaskResult>> allTasks = [];
 
-        while (true)
-        {
-            var runningCount = allTasks.Count(t => !t.IsCompleted);
-
-            var context = new LoadStrategyContext(PreviousResult: result, CurrentRunningTasks: runningCount);
-
-            result = options.LoadStrategy.GenerateLoad(LoadAction, context);
-
-            if (result.Tasks is { } tasks)
-            {
-                logger.LogDebug("Enqueueing {Count} new tasks", tasks.Count);
-
-                allTasks.AddRange(tasks.Select(task => Task.Run(() => task())));
-            }
-
-            if (result.RepeatDelay == null)
-            {
-                break;
-            }
-
-            await Task.Delay(result.RepeatDelay.Value);
-        }
-
-        logger.LogInformation("Waiting for {Count} tasks to complete...", allTasks.Count);
-
         List<LoadTaskResult> results = new(allTasks.Count);
 
-        foreach (var task in allTasks)
-        {
-            results.Add(await task);
-        }
+        await AnsiConsole.Progress()
+            .StartAsync(async (ctx) =>
+            {
+                var progressTask = ctx.AddTask("[green]Running Tasks...[/]", maxValue: allTasks.Count);
+
+                while (true)
+                {
+                    var runningCount = allTasks.Count(t => !t.IsCompleted);
+
+                    var context = new LoadStrategyContext(PreviousResult: result, CurrentRunningTasks: runningCount);
+
+                    result = options.LoadStrategy.GenerateLoad(LoadAction, context);
+
+                    if (result.Tasks is { } tasks)
+                    {
+                        logger?.LogDebug("Enqueueing {Count} new tasks", tasks.Count);
+
+                        allTasks.AddRange(tasks.Select(task => Task.Run(async () =>
+                        {
+                            var taskResult = await task();
+                            progressTask.Increment(1);
+                            progressTask.Description = $"[green]Running Tasks ({progressTask.Value}/{progressTask.MaxValue})[/]";
+                            return taskResult;
+                        })));
+                        progressTask.MaxValue += tasks.Count;
+                    }
+
+                    if (result.RepeatDelay == null)
+                    {
+                        break;
+                    }
+
+                    await Task.Delay(result.RepeatDelay.Value);
+                }
+
+                logger?.LogInformation("Waiting for {Count} tasks to complete...", allTasks.Count);
+
+                foreach (var task in allTasks)
+                {
+                    results.Add(await task);
+                }
+            });
 
         var successCount = results.Count(r => r.IsSuccess);
         var totalCount = results.Count;
         var passRate = successCount / (double)totalCount;
-        logger.LogInformation("Passed: {SuccessCount}/{TotalCount} ({PassRate:P1})", successCount, totalCount, passRate);
+        logger?.LogInformation("Passed: {SuccessCount}/{TotalCount} ({PassRate:P1})", successCount, totalCount, passRate);
+        AnsiConsole.MarkupLineInterpolated($"[green]Passed: {successCount}/{totalCount} ({passRate:P1})[/]");
 
         if (successCount != totalCount)
         {
@@ -71,21 +84,31 @@ public static class LoadTestRunner
                 .Select(g => (Type: g.Key, Count: g.Count()))
                 .OrderByDescending(g => g.Count);
 
-            logger.LogInformation("Exceptions by type:");
+            var table = new Table()
+                .BorderColor(Color.Red);
+
+            table.AddColumn("[red]Exception Type[/]");
+            table.AddColumn("[red]Count[/]");
 
             foreach (var (exceptionType, count) in exceptionCountsByType)
             {
-                logger.LogInformation("{ExceptionType}: {Count}", exceptionType.Name, count);
+                table.AddRow(
+                    new Markup(exceptionType.Name, new Style(foreground: Color.Red)),
+                    new Markup(count.ToString(), new Style(foreground: Color.Red)));
             }
+
+            AnsiConsole.Write(table);
         }
 
         var avgDuration = TimeSpan.FromMilliseconds(results
             .Select(r => r.Duration.TotalMilliseconds)
             .Average());
 
-        logger.LogInformation("Average duration: {AvgDuration}", avgDuration);
+        logger?.LogInformation("Average duration: {AvgDuration}", avgDuration);
+        AnsiConsole.MarkupLineInterpolated($"[green]Average duration: {avgDuration}[/]");
 
-        logger.LogInformation("Rucksack has finished");
+        logger?.LogInformation("Rucksack has finished");
+        AnsiConsole.MarkupLine("Rucksack has finished");
 
         return;
 
